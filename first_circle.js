18048eb1a92d0db65789ca23d4d40f72d8445435
Jordan Widdison <jordan.widdison@gmail.com>
2026-03-04 22:28:03 -0700

    Initial commit

---
+++ first_circle.js
@@ -0,0 +1,45 @@
+setcpm(60/4)
+cat(
+  sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+  sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon")
+  ),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon")
+  ),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon"),
+    note("g4@2 - g4 - c4@2  - c4 - c4 - g4@2 - g4@2 - a4 - f4 -").sound("gm_voice_oohs")
+  ),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon"),
+    note("g4@2 - g4 - c4@3 rest@14").sound("gm_voice_oohs")
+  ),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon"),
+    note("g4@2 - g4 - c4@4 rest@3 g4@2 - g4@2 - a4 - f4 a4").sound("gm_voice_oohs")
+  ),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon"),
+    note("c5@5 rest@15 c4 d4").sound("gm_voice_oohs")
+  ),
+  stack(
+    sound("- cp cp - cp - cp cp - cp - cp - cp cp - cp cp - cp - cp").gain(0.5),
+    note("c6 - - c6 - c6 - - c6 - c6 - c6 - - c6 - - c6 - c6 -, c4 - - c4 - c4 - - c4 - c4 - c4 - - c4 - - c4 - c4 -").sound("piano"),
+    note("c5 - - c5 - c5 - - c5 - c5 - c5 - - c5 - - c5 - c5 -, c3 - - c3 - c3 - - c3 - c3 - c3 - - c3 - - c3 - c3 -").sound("gm_acoustic_guitar_nylon"),
+    note("e4@3 f4@2 g4@4 rest@11").sound("gm_voice_oohs")
+  ),
+)
